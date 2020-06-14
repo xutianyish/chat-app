c150758e9c5bf03877ce68d526c3e550c1ae5076
xutianyish <xutianyish@gmail.com>
2020-06-14 18:55:07 -0400

    updated asgi file

--- mysite_project/asgi.py
+++ mysite_project/asgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
 from channels.layers import get_channel_layer
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite_project.settings')
+django.setup()
+application = get_default_application()
 channel_layer = get_channel_layer()
