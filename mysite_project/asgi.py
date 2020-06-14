5f8e3cfbdb4305e56fbf4034e19eeb6d7fc20aed
xutianyish <xutianyish@gmail.com>
2020-06-14 18:37:33 -0400

    updated pipenv

--- mysite_project/asgi.py
+++ mysite_project/asgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-import django
-from channels.routing import get_default_application
+import channels.asgi
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite_project.settings')
-django.setup()
-application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
