6a04a2b9c7dec5ebc60311a148c017a67e620e89
xutianyish <xutianyish@gmail.com>
2020-06-14 20:32:36 -0400

    update

--- mysite_project/settings.py
+++ mysite_project/settings.py
@@ -76,7 +76,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [os.env.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
     },
 }
