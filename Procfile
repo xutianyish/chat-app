8f6b3645bd6ad2455cf3a52117d4c43e3a30a5d6
xutianyish <xutianyish@gmail.com>
2020-06-14 20:11:30 -0400

    remove /chat/ in URL path

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: daphne mysite_project.asgi:application
-worker: python manage.py runworker -v2
--- mysite_project/settings.py
+++ mysite_project/settings.py
@@ -76,7 +76,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis-server-name', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
